cbf3f01d57bae517c7e6df519b9ad0a1c9c1eb81
berenice02 <foderaro.elisa@gmail.com>
2021-03-18 00:52:13 +0100

    base

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+# Import flask and template operators
+from flask import Flask, render_template
+
+# Define the application
+app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+
+#initial general page
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+# Import the modules using their blueprint handler variable
+# from app.mod_process.controllers import mod_process as process
+# from app.mod_shopfloor.controllers import mod_shopfloor as sf
+# from app.mod_products.controllers import mod_products as products
+# from app.mod_demands.controllers import mod_demands as demands
+
+# Register blueprint
+# app.register_blueprint(process)
+# app.register_blueprint(sf)
+# app.register_blueprint(products)
+# app.register_blueprint(demands)
