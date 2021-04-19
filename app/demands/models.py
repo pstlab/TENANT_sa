814e017a975a3efa97ec6573e243ab26154a52c6
berenice02 <foderaro.elisa@gmail.com>
2021-04-19 14:37:20 +0200

    minor changes

--- app/demands/models.py
+++ app/demands/models.py
@@ -19,3 +19,6 @@ class Demand(Base):
     # ManyToOne without bidirectional behavior
     #process_id = Column(Integer, ForeignKey('processes.id'))
     #process = relationship("Process")
+
+    def __str__(self):
+        return self.name
